4d9f7c2f7d5173c047a31f0b218b89b100b97c98
EJcoding <elijahespinosaaa@gmail.com>
2025-09-18 11:10:11 -0500

    implemented CORS

--- backend/main.py
+++ backend/main.py
@@ -2,11 +2,25 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:5173",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Input(BaseModel):
     text: str
-
-app = FastAPI()
 
 
 @app.get("/")
